487e2ee391588f01561f707b8f910c41eef6edab
Keyrisson <keyrisson.sibert@escola.pr.gov.br>
2025-06-26 08:36:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,30 +6,49 @@ let numero_dois = parseInt(document.getElementById('segundo_numero').value);
 let soma_numero = numero_um + numero_dois;
 
 document.getElementById("resultado").textContent= 'Resultado:' + soma_numero;
-function soma(){
+}
+function subtrair(){
 
 let numero_um =  parseInt(document.getElementById('primeiro_numero').value);
 let numero_dois = parseInt(document.getElementById('segundo_numero').value);
 
-let soma_numero = numero_um + numero_dois;
+let subtrair_numero = numero_um - numero_dois;
 
-document.getElementById("resultado").textContent= 'Resultado:' + soma_numero;
-
+document.getElementById("resultado").textContent= 'Resultado:' + subtrair_numero;   
 }
-function subtrair(){
- let numero_um =  parseInt(document.getElementById('primeiro_numero').value);
+function multiplicacao(){
+
+let numero_um =  parseInt(document.getElementById('primeiro_numero').value);
 let numero_dois = parseInt(document.getElementById('segundo_numero').value);
 
-let soma_numero = numero_um - numero_dois;
+let multiplicacao_numero = numero_um * numero_dois;
 
-document.getElementById("resultado").textContent= 'Resultado:' + subtrair;   
+document.getElementById("resultado").textContent= 'Resultado:' + multiplicacao_numero;   
 }
-}
-function subtrair(){
- let numero_um =  parseInt(document.getElementById('primeiro_numero').value);
+function dividir(){
+
+let numero_um =  parseInt(document.getElementById('primeiro_numero').value);
 let numero_dois = parseInt(document.getElementById('segundo_numero').value);
 
-let soma_numero = numero_um - numero_dois;
+if(numero_dois == 0){
+    alert("não pode dividir por zero");
+    return
+}
+let dividir_numero = numero_um / numero_dois;
 
-document.getElementById("resultado").textContent= 'Resultado:' + subtrair;   
+document.getElementById("resultado").textContent= 'Resultado:' + dividir_numero;   
+}
+function maior(){
+    
+    let numero_um =  parseInt(document.getElementById('primeiro_numero').value);
+    let numero_dois = parseInt(document.getElementById('segundo_numero').value);
+    if(numero_um>numero_dois){
+        document.getElementById("resultado").textContent= 'O maior é ' + numero_um;
+        return;
+    }else if(numero_dois>numero_um){
+        document.getElementById("resultado").textContent= 'O maior é ' + numero_dois;
+        return;
+    }else{
+        document.getElementById("resultado").textContent= 'São iguai'; 
+    }
 }
